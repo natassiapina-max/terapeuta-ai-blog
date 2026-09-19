0d5bdab92de11a0b3caf340d1a595f964f76c4a5
natassiapina-max <natassiapina@gmail.com>
2026-09-19 17:10:10 -0300

    add local Clarity loader

---
+++ public/clarity-loader.js
@@ -0,0 +1,6 @@
+(function () {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.clarity.ms/tag/ykusnfxcgt';
+  document.head.appendChild(script);
+})();
