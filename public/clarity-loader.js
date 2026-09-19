6b79235b051696e6f3cd04621e636bfb47fefbd4
natassiapina-max <natassiapina@gmail.com>
2026-09-19 17:13:25 -0300

    Corrige Project ID do Clarity

--- public/clarity-loader.js
+++ public/clarity-loader.js
@@ -1,6 +1,6 @@
 (function () {
   var script = document.createElement('script');
   script.async = true;
-  script.src = 'https://www.clarity.ms/tag/ykusnfxcgt';
+  script.src = 'https://www.clarity.ms/tag/ykusnfcxgt';
   document.head.appendChild(script);
 })();
