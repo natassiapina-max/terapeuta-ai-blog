c6166b10b89cfe213bb65f9c3b6645183f529560
natassiapina-max <natassiapina@gmail.com>
2026-09-20 10:14:56 -0300

    fix: exclude admin from sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://blog.terapeutaai.com.br',
-  integrations: [sitemap({ filter: (page) => !page.includes('/preview/') })],
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('/preview/') && !page.includes('/admin')
+    })
+  ],
   trailingSlash: 'never'
 });
